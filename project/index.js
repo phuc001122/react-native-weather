f6925cedcc56ab667cd45260c5c7d660496b1322
NguyenSon <33917382+NguyenSon2310@users.noreply.github.com>
2019-10-23 22:35:59 +0700

    Delete File

--- project/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import App from './ItemList';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
