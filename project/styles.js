68c2a793fe3c899fb981291ec4041a4dff779da9
phuc001122 <phuc001122@gmail.com>
2019-10-29 17:19:14 +0700

    Create styles.js
    tách styles ra thành file riêng

---
+++ project/styles.js
@@ -0,0 +1,159 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+    wrapper: {
+    },
+    slide1: {
+      flex: 1,
+      backgroundColor: '#9DD6EB'
+    },
+    slide2: {
+      flex: 1,
+      backgroundColor: '#9DD6EB',
+    },
+  
+  
+    input:
+    {
+      width: '90%',
+      marginLeft: 15,
+      color: 'white',
+      backgroundColor: 'rgba(52, 52, 52, 0.4)',
+      alignSelf: 'baseline',
+      borderRadius: 30,
+      paddingLeft: 20,
+      marginVertical: 15,
+    },
+  
+    info_basic_today:
+    {
+      paddingLeft: 20,
+      marginLeft: 15,
+      paddingRight: 20,
+      width: '90%',
+      alignSelf: "baseline",
+      backgroundColor: 'rgba(52, 52, 52, 0.4)',
+      marginVertical: 10
+    },
+    
+    temp_now:
+    {
+      color: 'white',
+      fontSize: 60,
+      width: '58%'
+    },
+  
+    icon_temp_now:
+    {
+      width: 100,
+      height: 90,
+      flexDirection: "row-reverse",
+      
+    },
+  
+    rowbasic_1:
+    {
+      marginVertical: 10,
+      flexDirection: "row"
+    },
+  
+    rowbasic_2:
+    {
+      flexDirection: "row",
+      marginVertical: 15
+    },
+  
+    temp_low_high:
+    {
+      color: 'white',
+      flexDirection: "row-reverse",
+      alignItems: "baseline"
+    },
+  
+    info_temp:
+    {
+      color: 'white',
+      width: '55%'
+    },
+  
+    rowbasic_2_1:
+    {
+      flexDirection: "row",
+      width: '50%'
+    },
+   
+  
+  
+    info_detail_today:
+    {
+      paddingHorizontal: 20,
+      marginLeft: 15,
+      paddingRight: 20,
+      width: '90%',
+      backgroundColor: 'rgba(52, 52, 52, 0.4)',
+      marginVertical: 10,
+    },
+    
+  
+    icon_weather:
+    {
+      width: 27,
+      height: 27,
+    },
+  
+    detail_title:
+    {
+      color: 'white',
+      height: 25,
+      width: 100,
+      paddingLeft: 15
+    },
+  
+    detail_number:
+    {
+      color: 'white',
+      height: 25,
+      marginStart: '20%',
+      marginEnd: 10
+    },
+  
+    detail_row:
+    {
+      flexDirection: "row",
+      marginTop: 23,
+      borderBottomWidth: 1,
+      borderBottomColor: '#cecdcd69',
+      paddingBottom: 10
+    },
+
+
+
+
+    
+    //forecast
+    flatListItem: {
+      color: 'white',
+      padding: 10,
+      fontSize: 16,
+    },
+  
+    flatList:{
+      flex: 1,
+      flexDirection: "row",
+      backgroundColor: 'mediumseagreen'
+    },
+    
+    image:{
+      width: 60, 
+      height: 60, 
+      marginTop: 10
+    },
+    thanhpho:{
+      padding: 10,
+      fontSize: 16,
+      backgroundColor: "tomato",
+      color: "white",
+    },
+  })
+
+module.exports = styles;
