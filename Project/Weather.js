bc147440ba3f4d5f3e0db9d0b9f79067fe7d5ed9
phuc001122 <phuc001122@gmail.com>
2019-10-31 08:47:02 +0700

    Update Weather.js

--- Project/Weather.js
+++ Project/Weather.js
@@ -6,17 +6,21 @@ import {
   Image,
   ActivityIndicator,
   ImageBackground,
-  ScrollView,
-  Button
+  Alert,
+  TouchableOpacity
 } from 'react-native';
 import styles from './styles';
+import Swiper from 'react-native-swiper'
+import Forecast from './Forecast';
+import ForecastInfo from './ForecastInfo';
+import { ScrollView } from 'react-native-gesture-handler';
 
 export default class App extends Component {
   constructor(props){
     super(props);
     this.state ={ 
       isLoading: true,
-      city: 'Thành phố Hồ Chí Minh',
+      city: 'Thu Duc',
     }
   }
 
@@ -41,10 +45,14 @@ export default class App extends Component {
   componentDidMount(){
     this.getWeather();
   }
+  
+  gethello()
+  {
+    console.log(`ahii`);
+  }
 
   render(){
     const weather = this.state.dataSource;
-    const {navigate} = this.props.navigation;
     
     if(this.state.isLoading){
       return(
@@ -56,78 +64,82 @@ export default class App extends Component {
 
     return (
       <ImageBackground source={require('./img/day.jpg')} style={{width: '100%', height: '100%'}}>
-      <ScrollView>
-      <View style={styles.slide1}>
-      <TextInput 
-        placeholder={'Nhập thành phố...'} 
-        style={styles.input}
-        onChangeText = {(TextInputText) => this.setState({ city: TextInputText })}
-        onSubmitEditing={()=>{
-          this.getWeather();
-        }}   
-        onKeyPress={ (event) => {
-          if(event.nativeEvent.key == "Enter"){
+        <ScrollView>
+          <View style={styles.slide1}>
+          <View style={styles.head}>
+            <TextInput 
+            placeholder={'Nhập thành phố...'} 
+            style={styles.input}
+            onChangeText = {(TextInputText) => this.setState({ city: TextInputText })}
+            onSubmitEditing={()=>{
               this.getWeather();
-          } 
-      }}
-      >{weather.name}</TextInput>
-      <View style={styles.info_basic_today}>
+            }}   
+            onKeyPress={ (event) => {
+              if(event.nativeEvent.key == "Enter"){
+                  this.getWeather();
+              } 
+          }}
+          >{weather.name}</TextInput>
 
-        <View style={styles.rowbasic_1}>
-          <Text style={styles.temp_now}>{parseInt(weather.main.temp,10)}°C</Text>
-          <Image source={{uri: "http://openweathermap.org/img/wn/"+ weather.weather[0].icon + "@2x.png"}} style={styles.icon_temp_now}></Image>
-        </View>
-        <Text style={styles.info_temp}>{}</Text>
-        <View style={styles.rowbasic_2}>
-          <Text style={styles.info_temp}>{weather.weather[0].description}</Text>
-          
-          <View style={styles.rowbasic_2_1}>
-            <Image source={require('./img/up-arrow.png')} style={{width: 10, height: 14, marginTop: 3}}></Image>
-            <Text style={styles.temp_low_high}>  {parseInt(weather.main.temp_min,10)}°C    </Text>
-            <Image source={require('./img/down-arrow.png')} style={{width: 10, height: 14, marginTop: 3}}></Image>
-            <Text style={styles.temp_low_high}>  {parseInt(weather.main.temp_max,10)}°C   </Text>
+            <TouchableOpacity onPress={()=>this.gethello()}>
+              <Image source={require('./img/checked.png')} style={styles.button}></Image>
+            </TouchableOpacity>
+
           </View>
-        </View>
-      </View>
-
-      <View style={styles.info_detail_today}>
-        <View style={styles.detail_row}>
-          <Image source={require('./img/humidity.png')} style={styles.icon_weather}></Image>
-          <Text style={styles.detail_title}>Độ ẩm</Text>
-          <Text style={styles.detail_number}>{weather.main.humidity + "%"}</Text>
-        </View>
-        <View style={styles.detail_row}>
-          <Image source={require('./img/cloudcover.png')} style={styles.icon_weather}></Image>
-          <Text style={styles.detail_title}>Mây che phủ</Text>               
-          <Text style={styles.detail_number}>{weather.clouds.all + "%"}</Text>
-        </View>
-        <View style={styles.detail_row}>
-          <Image source={require('./img/view.png')} style={styles.icon_weather}></Image>
-          <Text style={styles.detail_title}>Tầm nhìn</Text>
-          <Text style={styles.detail_number}>{weather.visibility / 1000 + "km"}</Text>
-        </View>
-        <View style={styles.detail_row}>
-          <Image source={require('./img/meter.png')} style={styles.icon_weather}></Image>
-          <Text style={styles.detail_title}>Sức ép</Text>
-          <Text style={styles.detail_number}>{weather.main.pressure}hPa</Text>
-        </View>
-        <View style={styles.detail_row}>
-          <Image source={require('./img/uv-protection.png')} style={styles.icon_weather}></Image>
-          <Text style={styles.detail_title}>UV</Text>
-          <Text style={styles.detail_number}>5</Text>
-        </View>
-        <View style={styles.detail_row}>
-          <Image source={require('./img/windmill.png')} style={styles.icon_weather}></Image>
-          <Text style={styles.detail_title}>Gió</Text>
-          <Text style={styles.detail_number}>{weather.wind.speed + " m/s"}</Text>
-        </View>
-      </View>
-      <Button
-        title="Các ngày tiếp theo"
-        onPress={() => navigate('Forecast', {city: this.state.city})}
-      />
-      </View>
-      </ScrollView>
+            <View style={styles.info_basic_today}>
+      
+              <View style={styles.rowbasic_1}>
+                <Text style={styles.temp_now}>{parseInt(weather.main.temp,10)}°C</Text>
+                <Image source={{uri: "http://openweathermap.org/img/wn/"+ weather.weather[0].icon + "@2x.png"}} style={styles.icon_temp_now}></Image>
+              </View>
+              <Text style={styles.info_temp}>{}</Text>
+              <View style={styles.rowbasic_2}>
+                <Text style={styles.info_temp}>{weather.weather[0].description}</Text>
+                
+                <View style={styles.rowbasic_2_1}>
+                  <Image source={require('./img/up-arrow.png')} style={{width: 10, height: 14, marginTop: 3}}></Image>
+                  <Text style={styles.temp_low_high}>  {parseInt(weather.main.temp_min,10)}°C    </Text>
+                  <Image source={require('./img/down-arrow.png')} style={{width: 10, height: 14, marginTop: 3}}></Image>
+                  <Text style={styles.temp_low_high}>  {parseInt(weather.main.temp_max,10)}°C   </Text>
+                </View>
+              </View>
+            </View>
+      
+            <View style={styles.info_detail_today}>
+              <View style={styles.detail_row}>
+                <Image source={require('./img/humidity.png')} style={styles.icon_weather}></Image>
+                <Text style={styles.detail_title}>Độ ẩm</Text>
+                <Text style={styles.detail_number}>{weather.main.humidity + "%"}</Text>
+              </View>
+              <View style={styles.detail_row}>
+                <Image source={require('./img/cloudcover.png')} style={styles.icon_weather}></Image>
+                <Text style={styles.detail_title}>Mây che phủ</Text>               
+                <Text style={styles.detail_number}>{weather.clouds.all + "%"}</Text>
+              </View>
+              <View style={styles.detail_row}>
+                <Image source={require('./img/view.png')} style={styles.icon_weather}></Image>
+                <Text style={styles.detail_title}>Tầm nhìn</Text>
+                <Text style={styles.detail_number}>{weather.visibility / 1000 + "km"}</Text>
+              </View>
+              <View style={styles.detail_row}>
+                <Image source={require('./img/meter.png')} style={styles.icon_weather}></Image>
+                <Text style={styles.detail_title}>Áp suất</Text>
+                <Text style={styles.detail_number}>{weather.main.pressure} hPa</Text>
+              </View>
+              <View style={styles.detail_row}>
+                <Image source={require('./img/uv-protection.png')} style={styles.icon_weather}></Image>
+                <Text style={styles.detail_title}>UV</Text>
+                <Text style={styles.detail_number}>5</Text>
+              </View>
+              
+              <View style={styles.detail_row}>
+                <Image source={require('./img/windmill.png')} style={styles.icon_weather}></Image>
+                <Text style={styles.detail_title}>Gió</Text>
+                <Text style={styles.detail_number}>{weather.wind.speed + " m/s"}</Text>
+              </View>
+            </View>
+          </View>
+        </ScrollView>
       </ImageBackground>
         
     );
